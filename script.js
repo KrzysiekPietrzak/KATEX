bd75d0303e81dcfebd21c6abd916a3ffcb7feea7
KrzysiekPietrzak <56595451+KrzysiekPietrzak@users.noreply.github.com>
2021-11-16 11:19:08 +0100

    transfer from atom

---
+++ script.js
@@ -0,0 +1,10 @@
+var next_mach=document.getElementById('next_machine');
+
+next_mach.onclick = function(){
+  alert("redd");
+
+/*var next_mach_number =   (parseInt(next_mach.getAttribute('ind_value'))+parseInt(1) )% parseInt(3)  ;
+alert(next_mach_number);
+next_mach.setAttribute('ind_value',next_mach_number);
+*/
+};
